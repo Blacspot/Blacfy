ccefaa7029bbf2cf2616aab69c8350fa4ca7cf07
blacspot <obwogemcbride73@gmail.com>
2023-09-26 10:54:21 +0300

    3dots

---
+++ blacfy/lib/features/todo/controllers/todo/todo_provider.dart
@@ -0,0 +1,30 @@
+import 'package:blacfy/common/helpers/db_helper.dart';
+import 'package:blacfy/common/models/task_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'todo_provider.g.dart';
+
+@riverpod
+class TodoState extends _$TodoState {
+  @override
+  List<Task> build() {
+    return [];
+  }
+
+  void refresh() async {
+    final data = await DBHelper.getItems();
+
+    state = data.map((e) => Task.fromJson(e)).toList();
+  }
+
+  void addItem(Task task) async {
+    await DBHelper.createItem(task);
+    refresh();
+  }
+
+  void updateItem(int id, String title, String desc, int isCompleted,
+      String date, String startTime, String endTime) async {
+    await DBHelper.updateItem(
+        id, title, desc, isCompleted, date, startTime, endTime);
+    refresh();
+  }
+}
--- blacfy/lib/main.dart
+++ blacfy/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:blacfy/common/utils/constants.dart';
 import 'package:blacfy/features/onboarding/pages/onboarding.dart';
 import 'package:blacfy/features/onboarding/widgets/page_two.dart';
 import 'package:blacfy/features/todo/pages/homepage.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -15,6 +16,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static final defaultLightColorScheme =
+      ColorScheme.fromSwatch(primarySwatch: Colors.blue);
+
+  static final defaultDarkColorScheme = ColorScheme.fromSwatch(
+      brightness: Brightness.dark, primarySwatch: Colors.blue);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -23,17 +30,25 @@ class MyApp extends StatelessWidget {
         designSize: const Size(375, 825),
         minTextAdapt: true,
         builder: (context, child) {
-          return MaterialApp(
-            title: 'Blacfy',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              scaffoldBackgroundColor: AppConst.kBkDark,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            themeMode: ThemeMode.dark,
-            home: const HomePage(),
-          );
+          return DynamicColorBuilder(
+              builder: (lightColorScheme, darkColorScheme) {
+            return MaterialApp(
+              title: 'Blacfy',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                scaffoldBackgroundColor: AppConst.kBkDark,
+                colorScheme: lightColorScheme ?? defaultLightColorScheme,
+                useMaterial3: true,
+              ),
+              darkTheme: ThemeData(
+                colorScheme: darkColorScheme ?? defaultDarkColorScheme,
+                scaffoldBackgroundColor: AppConst.kBkDark,
+                useMaterial3: true,
+              ),
+              themeMode: ThemeMode.dark,
+              home: const HomePage(),
+            );
+          });
         });
   }
 }
